c5a3d4edea23cb2a15d254c015d52c9660191e8c
Denis <jd.acemen@gmail.com>
2019-08-17 03:30:09 +0800

    Секция whyme и правая кнопка слайдера

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let list = [
-    'img/site-example/s1.webp',
+    'img/site-example/s1.png',
     'img/site-example/s2.jpg',
     'img/site-example/s3.jpg'
 ];
@@ -7,12 +7,28 @@ let listLen = list.length;
 let bg = document.getElementById('slider');
 let counter = 0;
 
-function slide() {
+function slideLeft() {
+  if (counter > 0) {
+    counter -= 1;
+    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter-1] + ')';
+    console.log(counter);
+  } else {
+    counter = listLen;
+    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter - 1] + ')';
+    
+    
+    console.log(counter);
+  }
+}
+
+function slideRight() {
   if (counter < listLen) {
     document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';  
     counter += 1;
     console.log(counter);
   } else {
     counter = 0;
+    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';
+    console.log(counter);
   }
 }
