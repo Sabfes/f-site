79d69404e6bd573c5087a34cda5555573b870597
Denis <jd.acemen@gmail.com>
2019-08-18 03:03:49 +0800

    Убрал ошибки слайдера (сделал макс 3 примера)

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ let list = [
     'img/site-example/s2.jpg',
     'img/site-example/s3.jpg'
 ];
-let listLen = list.length;
+let listLen = 2;
 let bg = document.getElementById('slider');
 let counter = 0;
 
 function slideLeft() {
   if (counter > 0) {
     counter -= 1;
-    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter-1] + ')';
+    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';
     console.log(counter);
   } else {
-    counter = listLen;
-    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter - 1] + ')';
+    counter = 2;
+    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';
     
     
     console.log(counter);
   }
 }
 function slideRight() {
-  if (counter < listLen) {
-    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';  
+  if (counter < 2) {
     counter += 1;
+    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';   
     console.log(counter);
   } else {
     counter = 0;
