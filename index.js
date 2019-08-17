d20e3b05ea0eecfac85032cfd05fb18e40eeeec1
Denis <jd.acemen@gmail.com>
2019-08-18 03:43:50 +0800

    + маркер открытого слайдера

--- index.js
+++ index.js
@@ -7,27 +7,50 @@ let listLen = 2;
 let bg = document.getElementById('slider');
 let counter = 0;
 
+let li = [
+  'li1',
+  'li2',
+  'li3'
+];
+
 function slideLeft() {
   if (counter > 0) {
     counter -= 1;
     document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';
+    document.getElementById(li[counter + 1]).style.backgroundColor = 'gray';
     console.log(counter);
   } else {
     counter = 2;
     document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';
-    
-    
+    document.getElementById(li[counter - 2]).style.backgroundColor = 'gray';
     console.log(counter);
   }
+  
+  if (counter != 0) {
+    document.getElementById(li[counter]).style.backgroundColor = 'yellow';
+  } else {
+    document.getElementById(li[counter]).style.backgroundColor = 'yellow';
+  }
+  
 }
 function slideRight() {
   if (counter < 2) {
     counter += 1;
-    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';   
+    document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')'; 
+    document.getElementById(li[counter - 1]).style.backgroundColor = 'gray';  
     console.log(counter);
   } else {
     counter = 0;
     document.getElementById('slider').style.backgroundImage = 'url(' + list[counter] + ')';
+    document.getElementById(li[counter + 2]).style.backgroundColor = 'gray';
     console.log(counter);
   }
+
+  if (counter == 2) {
+    document.getElementById(li[counter]).style.backgroundColor = 'yellow';
+  } else {
+    document.getElementById(li[counter]).style.backgroundColor = 'yellow';
+  }
+  
 }
+
