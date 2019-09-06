6b90d1f500f18cb65d61e676c1dcfc297221783f
Denis <jd.acemen@gmail.com>
2019-09-07 00:09:09 +0300

    +media to portfolio

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 window.addEventListener('scroll', function() {
-    var posY = window.pageYOffset;
+    let posY = window.pageYOffset;
     if (posY >= 100) {
         let a = document.getElementById('port-box-example1').style.transform = "translateX(132%)";  
         let b = document.getElementById('port-box-example2').style.transform = "translatex(-132%)";
         let c = document.getElementById('port-box-example3').style.transform = "translateX(132%)"; 
     }
 });
+
+
