089719d3cc6afaf8a2848fec93a4b2427490f8a2
= <jd.acemen@gmail.com>
2020-04-01 21:40:25 +0300

    +lesson

--- react-proj-1/src/App.js
+++ react-proj-1/src/App.js
@@ -4,21 +4,37 @@ import Car from './Car/Car.js';
 
 class App extends Component {
   state = {
+    divStyle: {
+      textAlign: 'center',
+    },
+
     cars: [
       {name: 'Ford', year: 2018},
       {name: 'Audi', year: 2016},
       {name: 'Mazda', year: 2010},
       {name: 'Mercedes-Benz CLA', year: 2020},
     ],
-    titleName: 'Cars',
+    pageTitle: 'Cars',
   }
+
+  changeTitleHandler = () => {
+    const oldTitle = this.state.pageTitle;
+    const newTitle = oldTitle + " (changed)";
+    
+    this.setState({
+      pageTitle: newTitle,
+    })
+  }
+
   render() {
     const cars = this.state.cars;
 
     return (
-      <div className="App">
-        <h1>{this.state.titleName}</h1>
-        <Car name={cars[0].name} year={cars[0].year} />
+      <div className="App" style={this.state.divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+
+        <button onClick={this.changeTitleHandler}>Change title</button>
+
         <Car name={cars[1].name} year={cars[1].year} />
         <Car name={cars[2].name} year={cars[2].year} />
         <Car name={cars[3].name} year={cars[3].year} />
