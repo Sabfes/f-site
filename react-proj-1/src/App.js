1ab75cbbc3c618e0f1b5851332ca94be4d3763a4
= <jd.acemen@gmail.com>
2020-04-01 21:06:28 +0300

    +lesson

--- react-proj-1/src/App.js
+++ react-proj-1/src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Car from './Car/Car.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-      
-      <Car name={'Ford'} year={2018} />
-      <Car name={'Audi'} year={2016} />
-      <Car name={'Mazda'} year={2010} />
-    </div>
-  );
+class App extends Component {
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010},
+      {name: 'Mercedes-Benz CLA', year: 2020},
+    ],
+    titleName: 'Cars',
+  }
+  render() {
+    const cars = this.state.cars;
 
-  // return React.createElement(
-  //   'div',
-  //   {
-  //     className: 'App',
-  //   },
-  //   React.createElement(
-  //     'h1',
-  //     null,
-  //     'Hello world!'
-  //   )
-  // )
-
+    return (
+      <div className="App">
+        <h1>{this.state.titleName}</h1>
+        <Car name={cars[0].name} year={cars[0].year} />
+        <Car name={cars[1].name} year={cars[1].year} />
+        <Car name={cars[2].name} year={cars[2].year} />
+        <Car name={cars[3].name} year={cars[3].year} />
+      </div>
+    )
+  };
 }
 
 export default App;
