b96b214e45443352bdc51e6a433bb27608e0474d
= <jd.acemen@gmail.com>
2020-04-03 23:30:21 +0300

    +lesson

--- react-proj-1/src/App.js
+++ react-proj-1/src/App.js
@@ -3,26 +3,28 @@ import './App.css';
 import Car from './Car/Car.js';
 
 class App extends Component {
-  state = {
-    divStyle: {
-      textAlign: 'center',
-    },
-    cars: [
-      {name: 'Ford', year: 2018},
-      {name: 'Audi', year: 2016},
-      {name: 'Mazda', year: 2010},
-      {name: 'Mercedes-Benz CLA', year: 2020},
-    ],
-    pageTitle: 'React component',
-    showCars: false,
-  };
-  //
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      cars: [
+        {name: 'Ford', year: 2018},
+        {name: 'Audi', year: 2016},
+        {name: 'Mazda', year: 2010},
+        {name: 'Mercedes-Benz CLA', year: 2020},
+      ],
+      pageTitle: 'React component',
+      showCars: false,
+    };
+  }
+  // Показ карточек
   toggleCarsHandler = () => {
     this.setState({
       showCars: !this.state.showCars,
     })
   };
-  //
+  // Удаление карты машины
   deleteHandler(index) {
     const carsArr = this.state.cars;
     delete carsArr[index];
@@ -30,7 +32,7 @@ class App extends Component {
       cars: carsArr,
     })
   }
-  //
+  // Обновление имени машины
   onChangeName = (name, index) => {
     const car = this.state.cars[index];
     car.name = name;
@@ -40,41 +42,37 @@ class App extends Component {
       cars: cars,
     })
   }
+  componentWillMount() {
+    console.log('app componentWillMount')
+  }
+  componentDidMount() {
+    console.log('app componentDidMount')
+  }
   // Рендер
   render() {
     return (
-      <div className="App" style={this.state.divStyle}>
-        <h1>{this.state.pageTitle}</h1>
+      <div className="App">
+        {/* <h1>{this.state.pageTitle}</h1> */}
+        <h1>{this.props.title}</h1>
 
         <button onClick={this.toggleCarsHandler}>Toggle cars</button>
 
-        { 
-          this.state.showCars 
-            ? this.state.cars.map( (car, index)=> {
-              return (
-                <Car 
-                  key={index}
-                  name={car.name} 
-                  year={car.year} 
-                  onDelete={this.deleteHandler.bind(this, index)} 
-                  onChangeName={ event => {this.onChangeName(event.target.value, index)} } />
-              )
-            })
-            : null
-        }
-
-        {/* <Car 
-          name={cars[1].name} 
-          year={cars[1].year} 
-          onChangeTitle={this.changeTitleHandler.bind(this, cars[1].name)} />
-        <Car 
-          name={cars[2].name} 
-          year={cars[2].year} 
-          onChangeTitle={this.changeTitleHandler.bind(this, cars[2].name)} />
-        <Car 
-          name={cars[3].name} 
-          year={cars[3].year} 
-          onChangeTitle={this.changeTitleHandler.bind(this, cars[3].name)} /> */}
+        <div className='carCard'>
+          { 
+            this.state.showCars 
+              ? this.state.cars.map( (car, index)=> {
+                return (
+                  <Car 
+                    key={index}
+                    name={car.name} 
+                    year={car.year} 
+                    onDelete={this.deleteHandler.bind(this, index)} 
+                    onChangeName={ event => {this.onChangeName(event.target.value, index)} } />
+                )
+              })
+              : null
+          }
+        </div>
       </div>
     )
   };
