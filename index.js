3942fe3dd3253f1f88f0924763da6f76c928b777
Denis <jd.acemen@gmail.com>
2021-02-26 20:54:53 +0300

    d

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var readline = require('readline');
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+import {stopSubmit} from "redux-form";
 
-let total = 0;
-process.stdin.on('end', () => { console.log(total); process.exit(0); });
+export const LoginUser = (data) => async (dispatch) => {
+    let res = await userApi.login(data)
 
-rl.on('line', function (data) {
-    data = data.split(' ');
-    total += parseInt(data[0]) || 0;
-    total += parseInt(data[1]) || 0;
-});
+    if (res.data.resultCode === 0) {
+        dispatch(authMe())
+    } else {
+        const msg = res.data.messages[0]
+        // название формы 'login' 
+        dispatch(stopSubmit('login', {_error: msg}))
+    }
+}
+
