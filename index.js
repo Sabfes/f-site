fa9837b48fe416a78e332138d81c2dae6d4d25c4
Denis <jd.acemen@gmail.com>
2021-04-14 21:34:31 +0300

    add last proj

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 import {stopSubmit} from "redux-form";
-
-export const LoginUser = (data) => async (dispatch) => {
-    let res = await userApi.login(data)
-
-    if (res.data.resultCode === 0) {
-        dispatch(authMe())
-    } else {
-        const msg = res.data.messages[0]
-        // название формы 'login' 
-        dispatch(stopSubmit('login', {_error: msg}))
-    }
-}
-
